e6c50f2dddd6b743741d07a392a5636d70e634db
Owen Kuemerle <kuemerle5@hotmail.com>
2013-02-04 16:15:48 -0600

    Add tribonacci files from class

---
+++ loops/tribonacci1.py
@@ -0,0 +1,67 @@
+(third, second, first, result) = 0, 0, 1, 0
+result = first + second + third
+first = second
+second = third
+third = result
+print "Result: ", result
+result = first + second + third
+first = second
+second = third
+third = result
+print "Result: ", result
+result = first + second + third
+first = second
+second = third
+third = result
+print "Result: ", result
+result = first + second + third
+first = second
+second = third
+third = result
+print "Result: ", result
+result = first + second + third
+first = second
+second = third
+third = result
+print "Result: ", result
+result = first + second + third
+first = second
+second = third
+third = result
+print "Result: ", result
+result = first + second + third
+first = second
+second = third
+third = result
+print "Result: ", result
+result = first + second + third
+first = second
+second = third
+third = result
+print "Result: ", result
+result = first + second + third
+first = second
+second = third
+third = result
+print "Result: ", result
+result = first + second + third
+first = second
+second = third
+third = result
+print "Result: ", result
+result = first + second + third
+first = second
+second = third
+third = result
+print "Result: ", result
+result = first + second + third
+first = second
+second = third
+third = result
+print "Result: ", result
+result = first + second + third
+first = second
+second = third
+third = result
+print "Result: ", result
+print "The result of the first 15 tribonacci numbers: ", result
