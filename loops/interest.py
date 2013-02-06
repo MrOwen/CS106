99c71820197b7e09b902b1d9d19932b06694c10a
Owen K. <kuemerle5@hotmail.com>
2013-02-06 12:10:25 -0600

    Added loop indicator

--- loops/interest.py
+++ loops/interest.py
@@ -1,6 +1,8 @@
 (principle, interest_rate, interest) = 1234, 5, 0
+# Begin loop
 principle = (1 + (interest_rate / 100.0)) * principle
 print "Current amount: $%.2f" % round(principle, 2)
+# End loop
 principle = (1 + (interest_rate / 100.0)) * principle
 print "Current amount: $%.2f" % round(principle, 2)
 principle = (1 + (interest_rate / 100.0)) * principle
