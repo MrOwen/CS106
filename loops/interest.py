b135d55e0c4ec1d4a2cb14054429bf43250f81a6
Owen K. <kuemerle5@hotmail.com>
2013-02-06 12:09:07 -0600

    Added/fixed tribonacci programs, added interest program

---
+++ loops/interest.py
@@ -0,0 +1,41 @@
+(principle, interest_rate, interest) = 1234, 5, 0
+principle = (1 + (interest_rate / 100.0)) * principle
+print "Current amount: $%.2f" % round(principle, 2)
+principle = (1 + (interest_rate / 100.0)) * principle
+print "Current amount: $%.2f" % round(principle, 2)
+principle = (1 + (interest_rate / 100.0)) * principle
+print "Current amount: $%.2f" % round(principle, 2)
+principle = (1 + (interest_rate / 100.0)) * principle
+print "Current amount: $%.2f" % round(principle, 2)
+principle = (1 + (interest_rate / 100.0)) * principle
+print "Current amount: $%.2f" % round(principle, 2)
+principle = (1 + (interest_rate / 100.0)) * principle
+print "Current amount: $%.2f" % round(principle, 2)
+principle = (1 + (interest_rate / 100.0)) * principle
+print "Current amount: $%.2f" % round(principle, 2)
+principle = (1 + (interest_rate / 100.0)) * principle
+print "Current amount: $%.2f" % round(principle, 2)
+principle = (1 + (interest_rate / 100.0)) * principle
+print "Current amount: $%.2f" % round(principle, 2)
+principle = (1 + (interest_rate / 100.0)) * principle
+print "Current amount: $%.2f" % round(principle, 2)
+principle = (1 + (interest_rate / 100.0)) * principle
+print "Current amount: $%.2f" % round(principle, 2)
+principle = (1 + (interest_rate / 100.0)) * principle
+print "Current amount: $%.2f" % round(principle, 2)
+principle = (1 + (interest_rate / 100.0)) * principle
+print "Current amount: $%.2f" % round(principle, 2)
+principle = (1 + (interest_rate / 100.0)) * principle
+print "Current amount: $%.2f" % round(principle, 2)
+principle = (1 + (interest_rate / 100.0)) * principle
+print "Current amount: $%.2f" % round(principle, 2)
+principle = (1 + (interest_rate / 100.0)) * principle
+print "Current amount: $%.2f" % round(principle, 2)
+principle = (1 + (interest_rate / 100.0)) * principle
+print "Current amount: $%.2f" % round(principle, 2)
+principle = (1 + (interest_rate / 100.0)) * principle
+print "Current amount: $%.2f" % round(principle, 2)
+principle = (1 + (interest_rate / 100.0)) * principle
+print "Current amount: $%.2f" % round(principle, 2)
+principle = (1 + (interest_rate / 100.0)) * principle
+print "Final amount: $%.2f" % round(principle, 2)
--- loops/tribonacci3.py
+++ loops/tribonacci3.py
@@ -1,4 +1,3 @@
-limit_reached = False
 (third, second, first, result) = 0, 0, 1, 0
 result = first + second + third
 first = second
@@ -46,9 +45,27 @@ second = third
 third = result
 print "Result: ", result
 result = first + second + third
-limit_reached = True
 first = second
 second = third
 third = result
 print "Result: ", result
-print "Limit reached"
+result = first + second + third
+first = second
+second = third
+third = result
+print "Result: ", result
+result = first + second + third
+first = second
+second = third
+third = result
+print "Result: ", result
+result = first + second + third
+first = second
+second = third
+third = result
+print "Result: ", result
+result = first + second + third
+first = second
+second = third
+third = result
+print "Result: ", result
